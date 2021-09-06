c7f678bfabe2f3cdc7f615419fb75b0231a0c06e
King-sx-ID <89509394+King-sx-ID@users.noreply.github.com>
2021-09-06 14:54:00 +0700

    Add files via upload

---
+++ hack-pulsa.sh
@@ -0,0 +1,73 @@
+clear
+#Waduh Ada Kang Recode Nih
+#Tapi Gpp Hanya Hiburan Wkwkwk
+#Visit : > blackhatusa.gosch.id
+#jangan Recode njir ini asli script
+#Hack pulsa Awas aja lu recode wkwkwk
+
+#Kode Kang Recode
+
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1'
+
+echo -e $blue
+
+figlet -f slant "Hack Pulsa"
+
+sleep 1
+echo -e $yellow
+
+echo "##############################"
+echo "PEMBUAT SCRIPT : BLACK HAT USA"
+echo "WEBSITE : blackhatusa.gosch.id"
+echo "NO WA : +1 (512) 994-1864"
+echo "##############################"
+
+sleep 1
+
+echo -e $red
+
+read -p " Masukan Nama Kartu anda indosat: " kartu
+
+read -p " Masukan Nomor Lu: " no
+
+read -p " Jumlah Pulsa Yang Kamu inginkan: " jum
+sleep 2
+echo "Tunggu Bro Sedang Mengcek"
+sleep 10
+echo -e $white
+echo "------------------------------"
+echo "Kartu anda: $kartu "
+echo "Nomor Lu: $no "
+echo "Jumlah Pulsa: $jum "
+echo "------------------------------"
+sleep 1
+echo "Sedang Proses 10%"
+sleep 1
+echo "Sedang Proses 20%"
+sleep 1
+echo "Sedang Proses 30%"
+sleep 1
+echo "Sedang Proses 40%"
+sleep 1
+echo "Sedang Proses 50%"
+sleep 1
+echo "Sedang Proses 60%"
+sleep 1
+echo "Sedang Proses 70%"
+sleep 1
+echo "Sedang Proses 80%"
+sleep 1
+echo "Sedang Proses 90%"
+sleep 1
+echo "Selesai 100%"
+sleep 3
+echo "----------------------------------------------------"
+echo "Selamat $no Mendapatkan Pulsa $jum Ribu"
+echo "----------------------------------------------------"
+sleep 1
